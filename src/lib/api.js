be9f4922a108df4e836c238a828d82135588ca9f
lowrider-fn <sergei.dolmatskii@gmail.com>
2022-01-02 20:18:47 +0300

    docs

--- src/lib/api.js
+++ src/lib/api.js
@@ -3,9 +3,11 @@ import axios from 'axios';
 const outErr = err => ({ res: null, err });
 const outRes = res => ({ res, err: null });
 
+const baseUrl = process.env.NODE_ENV === 'production' ? '/gant' : '';
+
 const post = async (data) => {
     try {
-        const { res } = await axios.post('/post/data.json', data);
+        const { res } = await axios.post(`${baseUrl}/post/data.json`, data);
         if (res.err) throw res;
         outRes(res);
     } catch (err) {
@@ -15,8 +17,8 @@ const post = async (data) => {
 
 const get = async () => {
     try {
-        const { data } = await axios.get('/get/data.json');
-        if (data.err)  throw data;
+        const { data } = await axios.get(`${baseUrl}/get/data.json`);
+        if (data.err) throw data;
         return outRes(data);
     } catch (err) {
         console.error(err);
